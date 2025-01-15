e8ac918c02a64f97b6451b2c0b056fd3bfb86082
최도현 <chlehgus212@naver.com>
2025-01-15 16:45:58 +0900

    sample

--- src/main/java/com/immunologyHomepage/config/WebSecurityConfig.java
+++ src/main/java/com/immunologyHomepage/config/WebSecurityConfig.java
@@ -2,11 +2,12 @@ package com.immunologyHomepage.config;
 
 import java.io.IOException;
 
-import org.springframework.beans.factory.annotation.Configurable;
+
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;  
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,7 +26,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
-@Configurable
+@Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
@@ -44,10 +45,11 @@ public class WebSecurityConfig {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(request -> request
-                .requestMatchers("/","/api/v1/auth/**", "/api/v1/search/**", "/file/**").permitAll()
+                .requestMatchers("/","/api/v1/auth/**", "/file/**").permitAll()
                 .requestMatchers(HttpMethod.GET, "/api/v1/board/**", "/api/v1/user/*").permitAll()  // 특정 경로 허용
                 .anyRequest().authenticated()  // 그 외 요청은 인증 필요
             )
+            
             .exceptionHandling(exceptionHandling -> exceptionHandling
             .authenticationEntryPoint(new FailedAuthenticationEntryPoint())
             )
