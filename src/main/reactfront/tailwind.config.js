6bc7fd0ce58bee55d90d9235ec758251defe4501
unknown <dngh741@gmail.com>
2025-01-16 15:14:48 +0900

    feature#101 : 사이드바 인포카드 실험실소개글(About us)

--- src/main/reactfront/tailwind.config.js
+++ src/main/reactfront/tailwind.config.js
@@ -13,3 +13,18 @@ module.exports = {
   },
   plugins: [],
 };
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        cardBlue: "#B3E5FC", // 파스텔 하늘색
+        cardOrange: "#FFE0B2", // 파스텔 주황색
+        cardGreen: "#C8E6C9", // 파스텔 초록색
+        backgroundGreen: "#F1F8E9", // 파스텔 민트/연두색 (배경색)
+      },
+    },
+  },
+  plugins: [],
+};
