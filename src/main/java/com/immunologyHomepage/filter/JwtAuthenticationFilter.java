5650ac9592e52c4b8258d382087ca48630d051c5
최도현 <chlehgus212@naver.com>
2025-01-17 15:05:15 +0900

    debug - 회원가입, 로그인 api 디버깅 완료 : spring security문제

--- src/main/java/com/immunologyHomepage/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/immunologyHomepage/filter/JwtAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package com.immunologyHomepage.filter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -12,7 +16,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.immunologyHomepage.entity.AdminEntity;
 import com.immunologyHomepage.provider.JwtProvider;
+import com.immunologyHomepage.repository.AdminRepository;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -25,10 +31,18 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationFilter extends OncePerRequestFilter{
     
     private final JwtProvider jwtProvider;
-
+    private final AdminRepository adminRepository;
+    
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
+                
+                String requestPath = request.getRequestURI();
+
+                if (requestPath.startsWith("/api/v1/auth")) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
 
         try{
             String token = parseBearerToken(request);
@@ -45,17 +59,27 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
             return;
         }
 
+        AdminEntity adminEntity = adminRepository.findByUserName((userName));
+        String role = adminEntity.getRole(); //ROLE_USER, ROLE_ADMIN
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+
+        //ROLE_DEVELOPER ROLE_BOSS
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role));
+
         AbstractAuthenticationToken authenticationToken =
-            new UsernamePasswordAuthenticationToken(userName, null, AuthorityUtils.NO_AUTHORITIES);
+            new UsernamePasswordAuthenticationToken(userName, null, authorities);
 
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 
-            SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+           
             securityContext.setAuthentication(authenticationToken);
-
             SecurityContextHolder.setContext(securityContext);
 
         }catch(Exception exception){
+            System.out.println("JWT Authentication failed");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             exception.printStackTrace();
         }
         filterChain.doFilter(request, response);
--- src/main/java/com/immunologyHomepage/immunologyHomepage/ImmunologyHomepageApplication.java
+++ src/main/java/com/immunologyHomepage/immunologyHomepage/ImmunologyHomepageApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.immunologyHomepage")
 @EntityScan(basePackages = "com.immunologyHomepage.entity")
 @EnableJpaRepositories("com.immunologyHomepage.repository")
 public class ImmunologyHomepageApplication {
