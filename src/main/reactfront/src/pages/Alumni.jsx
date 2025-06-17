dfcf78a62e6a57dfc2f8390538ee8e17fec46a04
unknown <chlehgus212@naver.com>
2025-06-17 15:49:59 +0900

    new db

--- src/main/reactfront/src/pages/Alumni.jsx
+++ src/main/reactfront/src/pages/Alumni.jsx
@@ -81,9 +81,6 @@ export default function Alumni() {
     }
   };
 
-<<<<<<< HEAD
-  
-=======
   // degree 별로 멤버 그룹화
   const groupedMembers = members.reduce((acc, member) => {
     const { degree } = member;
@@ -93,34 +90,11 @@ export default function Alumni() {
     acc[degree].push(member);
     return acc;
   }, {});
->>>>>>> a46708ae3fed22194846e4998a39c114397bd832
 
   return (
     <div className="w-full mx-auto px-1 custom-mb:px-12 py-4">
       <div className="min-h-screen py-8">
         <div className="space-y-6">
-<<<<<<< HEAD
-          {members.map((member) => (
-            <div
-              key={member.boardNumber}
-              className="bg-white shadow-md rounded-lg overflow-hidden hover:shadow-lg transition-shadow duration-300 flex flex-col md:flex-row items-center"
-            >
-              {/* 이미지 섹션 */}
-              <div className="custom-mb:w-48 h-48 w-48 flex-shrink-0 p-4">
-                <img
-                  src={member.image}
-                  alt={member.name}
-                  className="w-full h-full object-cover"
-                />
-              </div>
-              
-              {/* 정보 섹션 */}
-              <div className="flex-grow p-6 flex flex-col md:flex-row justify-between items-center">
-                <div className="text-center md:text-left">
-                  <h2 className="text-2xl font-semibold mb-2">{member.name}</h2>
-                  <p className="text-gray-600 mb-1">{member.email}</p>
-                  <p className="text-gray-800">{member.degree}</p>
-=======
           {Object.keys(groupedMembers).map((degree) => (
             <div key={degree}>
               <h2 className="text-3xl font-semibold mb-4">{degree}</h2>
@@ -156,7 +130,6 @@ export default function Alumni() {
                       </button>
                     )}
                   </div>
->>>>>>> a46708ae3fed22194846e4998a39c114397bd832
                 </div>
               ))}
             </div>
